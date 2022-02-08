e6db523bb6f6c1e5beb6fa18c94652711f81e9e9
tejashreebhawari <99281810+tejashreebhawari@users.noreply.github.com>
2022-02-09 00:44:52 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+var React = require("react");
+
+var ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  //combining 2 html elements in single div
+  <div>
+    <h1>Hello World!</h1>
+    <p>This a Paragraph</p>
+  </div>,
+  document.getElementById("root")
+);
